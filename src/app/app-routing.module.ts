0ca1b21baa3286f9442da66965ee1f3b1279ef2b
Giordano Menezes <giordano.menezes@bol.com.br>
2018-12-24 17:21:16 -0300

    Versao Producao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import {Routes, RouterModule} from '@angular/router';
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
 const approutes: Routes = [
-    {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
-    { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
     {path: 'nao-autorizado', component: NaoAutorizadoComponent},
     {path: '**', redirectTo: 'pagina-nao-encontrada'}
  ];
