9c6996b180c337b58be1f930167a650f9d6b08e7
Giordano Menezes <giordano.menezes@bol.com.br>
2018-12-24 01:49:52 -0300

    Primeira Versao Producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  // apiEndpoint: 'https://gior-algamoney-api.herokuapp.com',
-  // tokenWhitelistedDomains: ['gior-algamoney-api.herokuapp.com'],
-  // tokenBlacklistedRoutes: ['https://gior-algamoney-api.herokuapp.com/oauth/token']
+  apiEndpoint: 'https://gior-algamoney.herokuapp.com',
+  tokenWhitelistedDomains: ['gior-algamoney.herokuapp.com'],
+  tokenBlacklistedRoutes: ['https://gior-algamoney.herokuapp.com/oauth/token']
 
-  apiEndpoint: 'http://localhost:8080',
-  tokenWhitelistedDomains: ['localhost:8080'],
-  tokenBlacklistedRoutes: ['http://localhost:8080/oauth/token']
+  // apiEndpoint: 'http://localhost:8080',
+  // tokenWhitelistedDomains: ['localhost:8080'],
+  // tokenBlacklistedRoutes: ['http://localhost:8080/oauth/token']
 };
